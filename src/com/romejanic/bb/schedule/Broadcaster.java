db67cc34f7a2e66234579618f4a425d1b1a96668
Jack Davenport <jack.davenport5011@gmail.com>
2020-04-18 23:44:15 +1000

    Added prefix to broadcasted messages

--- src/com/romejanic/bb/schedule/Broadcaster.java
+++ src/com/romejanic/bb/schedule/Broadcaster.java
@@ -1,6 +1,7 @@
 package com.romejanic.bb.schedule;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 
 import com.romejanic.bb.BasicBroadcast;
 import com.romejanic.bb.util.Util;
@@ -10,22 +11,22 @@ public class Broadcaster implements Runnable {
 	private final BasicBroadcast plugin;
 	private int scheduleID = -1;
 	
-	
-	
 	public Broadcaster(BasicBroadcast plugin) {
 		this.plugin = plugin;
 	}
 	
 	@Override
 	public void run() {
-		Bukkit.broadcastMessage("Broadcasting test");
+		String prefix = this.plugin.config.getChatPrefix();
+		Bukkit.broadcastMessage(prefix + ChatColor.RESET + " Broadcasting test");
 	}
 
 	public boolean schedule() {
 		long secs = Util.parsePeriodString(this.plugin.config.getBroadcastPeriod());
 		if(secs < 0l) return false;
-		secs *= 20L; // convert number of seconds into number of ticks
-		this.scheduleID = Bukkit.getScheduler().scheduleSyncRepeatingTask(this.plugin, this, 0l, secs);
+		long ticks = secs * 20L; // convert number of seconds into number of ticks
+		this.plugin.getLogger().info("Broadcasting every " + this.plugin.config.getBroadcastPeriod() + " (" + secs + "s, " + ticks + "t)");
+		this.scheduleID = Bukkit.getScheduler().scheduleSyncRepeatingTask(this.plugin, this, 0l, ticks);
 		return this.isActive();
 	}
 	
